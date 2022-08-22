f5092f3c9575221f077cb7e6aaec85eaec4a28d1
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-08-22 20:03:30 +0300

    added 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function makeGalleryMarkup(pictures) {
+  return pictures
+    .map(({ preview, original, description }) => {
+      return (
+        'beforeend',
+        `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+      );
+    })
+    .join('');
+}
+
+let clickedPicture = '';
+function makeInstance(e) {
+  e.preventDefault();
+  clickedPicture = e.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+      <img src="${clickedPicture}" width="800" height="600">
+  `);
+  instance.show();
+
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  } else {
+    document.addEventListener('keydown', e => {
+      if (e.code === 'Escape') {
+        instance.close();
+      }
+    });
+  }
+}
+
+galleryContainer.addEventListener('click', makeInstance);
 console.log(galleryItems);
